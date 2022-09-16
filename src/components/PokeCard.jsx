65b5f28159f1c7aec060e9e6196c19e104d5468c
Lugp <lugpdev2022@gmail.com>
2022-09-16 09:40:14 -0500

    Make stetic changes

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -23,7 +23,7 @@ export const PokeCard = ({ pokemon }) => {
     <div className='col-12 col-sm-6 col-lg-3 mt-4'>
       <div className='card card-img-top'>
         <img src={sprite} alt='Pokemon Image' />
-        <div className='card-body'>
+        <div className='card-body shadow'>
           <h4 className='card-title text-capitalize'>{name}</h4>
           <span className='card-text'>Index: {id}</span>
         </div>
