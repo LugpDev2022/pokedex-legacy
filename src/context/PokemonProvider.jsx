0f417b979bc5dc9995b72b6bf8b782f18a8a1fc8
Lugp <lugpdev2022@gmail.com>
2022-10-14 08:44:21 -0500

    Remove initializer function on context

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -2,16 +2,13 @@ import { useReducer } from 'react';
 import { PokemonContext, pokemonReducer, types } from './';
 
 export const PokemonProvider = ({ children }) => {
-  const init = () => {
-    const initialState = {
-      arePokemonsCharging: true,
-      pokemons: [],
-      page: 1,
-    };
-
-    return initialState;
+  const initialState = {
+    arePokemonsCharging: true,
+    pokemons: [],
+    page: 1,
   };
-  const [state, dispatch] = useReducer(pokemonReducer, {}, init);
+
+  const [state, dispatch] = useReducer(pokemonReducer, initialState);
 
   const chargePokemons = pokemons => {
     const action = {
